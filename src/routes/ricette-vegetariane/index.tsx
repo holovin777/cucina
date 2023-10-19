8dd574e038c60b992ebe5e7802a6ef25c99a9fe4
Viktor Holovin <holovin@mail.com>
2023-10-19 14:56:06 +0200

    Insalata di Ceci con Avocado e Pomodori Ciliegia

--- src/routes/ricette-vegetariane/index.tsx
+++ src/routes/ricette-vegetariane/index.tsx
@@ -4,6 +4,7 @@ import { Link, type DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
+      <h1><Link href="/ricette-vegetariane/insalata-di-ceci-con-avocado-e-pomodori-ciliegia/">Insalata di Ceci con Avocado e Pomodori Ciliegia</Link></h1>
       <h1><Link href="/ricette-vegetariane/pasta-con-verdure-miste/">Pasta con verdure miste</Link></h1>
       <h1><Link href="/ricette-vegetariane/quinoa-con-verdure-arrosto/">Quinoa con Verdure Arrosto</Link></h1>
     </>
