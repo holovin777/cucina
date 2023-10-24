25958d4788b381474ff06af38839fe047704fd44
Viktor Holovin <holovin@mail.com>
2023-10-24 12:36:56 +0200

    Fix error a base di carne

---
+++ src/routes/ricette/a-base-di-carne/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <ul>
+      <li>
+        <button>
+          <h1>
+            <Link href="/ricette/a-base-di-carne/involucri-di-prosciutto-cotto-e-philadelphia/">Frittata alle Erbe e Formaggio</Link>
+          </h1>
+        </button>
+      </li>
+    </ul>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Ricette",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
+
