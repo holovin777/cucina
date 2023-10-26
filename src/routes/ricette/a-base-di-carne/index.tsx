39da7e569fa1ab94f4187a45405656b598f12f60
Viktor Holovin <holovin@mail.com>
2023-10-26 12:07:15 +0200

    Add button in ricette a base di carne

--- src/routes/ricette/a-base-di-carne/index.tsx
+++ src/routes/ricette/a-base-di-carne/index.tsx
@@ -1,26 +1,24 @@
 import { component$ } from "@builder.io/qwik";
-import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
+import { Li } from "~/components/link/link";
 
 export default component$(() => {
   return (
     <ul>
-      <li>
-        <button>
-          <h1>
-            <Link href="/ricette/a-base-di-carne/involucri-di-prosciutto-cotto-e-philadelphia/">Involucri di Prosciutto cotto e Philadelphia</Link>
-          </h1>
-        </button>
-      </li>
+      <Li
+        href="/ricette/a-base-di-carne/involucri-di-prosciutto-cotto-e-philadelphia/"
+        name="Involucri di Prosciutto cotto e Philadelphia"
+      />
     </ul>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Ricette",
+  title: "Welcome to Ricette a base di carne",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Welcome to Ricette a base di carne",
     },
   ],
 };
